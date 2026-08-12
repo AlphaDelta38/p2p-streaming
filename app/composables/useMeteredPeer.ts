3e87c3d24c74ab5d3f029d881e7b62dc47857af8
alphadelta38 <kiril.morozov.01@gmail.com>
2026-08-12 23:18:25 +0300

    fix: stun server add

--- app/composables/useMeteredPeer.ts
+++ app/composables/useMeteredPeer.ts
@@ -45,7 +45,22 @@ export const useMeteredPeer = () => {
     }
 
     try {
-      peerInstance = new MeteredPeer({ apiKey })
+      peerInstance = new MeteredPeer({ 
+        apiKey,
+        rtcPeerConnectionFactory: (config: any) => {
+          const enrichedConfig = {
+            ...config,
+            iceServers: [
+              ...(config.iceServers || []),
+              { urls: 'stun:stun.l.google.com:19302' },
+              { urls: 'stun:stun1.l.google.com:19302' },
+              { urls: 'stun:stun2.l.google.com:19302' },
+              { urls: 'stun:stun3.l.google.com:19302' }
+            ]
+          }
+          return new RTCPeerConnection(enrichedConfig)
+        }
+      })
       setupPeerEvents(peerInstance)
       return peerInstance
     } catch (error) {
